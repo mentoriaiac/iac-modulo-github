26d8b2d37c485f71963a6f54fdff6a9e9e9001ef
Marcos Rezende <rehzende@hotmail.com>
2021-10-09 23:04:21 -0300

    refactor: adicionando uma variavel para receber o valor de organization

--- how-to-use-this-module/terrafile.tf
+++ how-to-use-this-module/terrafile.tf
@@ -1,6 +1,9 @@
+variable "organization" {
+  default = "teste-team-module"
+}
 module "github_teams_members" {
   source       = "../"
-  organization     = "mentoriaiactest"
+  organization     = var.organization
   team_members = ["user1", "user2"]
   team_name = "developers"
 }
